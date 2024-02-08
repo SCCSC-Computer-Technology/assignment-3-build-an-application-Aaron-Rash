6815822ca616f9b62ccd4a73a0fa7c24c9ba76ef
Aaron Rash <155991638+Aaron-Rash@users.noreply.github.com>
2024-02-07 21:58:55 -0500

    Add files via upload

---
+++ AaronRash-CPT-206-Lab-3/AaronRash-CPT-206-Lab-3/StateForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AaronRash_CPT_206_Lab_3
+{
+    public partial class StateForm : Form
+    {
+        public StateForm(string selectedState)
+        {
+            InitializeComponent();
+        }
+
+        private void StateInfoBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.stateInfoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.stateInfoDataSet);
+
+        }
+
+        private void StateForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'stateInfoDataSet.StateInfo' table. You can move, or remove it, as needed.
+            this.stateInfoTableAdapter.Fill(this.stateInfoDataSet.StateInfo);
+
+        }
+    }
+}
